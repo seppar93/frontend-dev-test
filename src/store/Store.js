4e6cbf0053ae7f7cd8f22aaafd0f557d052826df
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-18 15:54:04 -0400

    store finished

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,3 +2,8 @@ import TICTACTOE from "src/store/actions/index.js";
 
 import { createStore } from "redux";
 import logger from "redux-logger";
+import { applyMiddleware } from "redux";
+
+let sotre = createStore(TICTACTOE, applyMiddleware(logger));
+
+export default store;
