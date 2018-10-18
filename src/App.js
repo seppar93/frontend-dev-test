61512a80e3aa99cf72d90a7f9a1bdfcfd1d46b36
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-18 12:04:18 -0400

    game logic in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
-const mapStateToProps = function(state,props) {
-  return {}
-}
+const mapStateToProps = function(state, props) {
+  return {};
+};
 
-const mapDispatchToProps = dispatch => ({
-  
-});
+const mapDispatchToProps = dispatch => ({});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null),
+      player: "X"
+    };
+  }
+  handleClick(index) {
+    let newBoard = this.state.board;
+    newBoard[index] = this.state.player;
+
+    this.setState({
+      board: newBoard,
+      player: this.state.player === "X" ? "O" : "X"
+    });
+    console.log(this.state.board);
+
+    console.log(index);
+  }
   render() {
+    const Box = this.state.board.map((box, index) => (
+      <div className="box" key={index} onClick={e => this.handleClick(index)}>
+        {box}
+      </div>
+    ));
+
     return (
-      <div className="App">
-        <h1 className="App-title">A Tic Tac Toe Game will be created</h1>
+      <div className="container">
+        <h1>Tic Tac Toe </h1>
+        <div className="board">
+          {Box}
+          {/* <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div>
+          <div className="box"> X </div> */}
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
