e373987cf042afb4a6fdf509c4455b1f32ab402f
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-21 16:50:40 -0400

    functional game

---
+++ src/components/Board.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Board extends Component {
+  render() {
+    const Box = this.props.rows.map((row, rowIndex) => (
+      <div className="row" key={rowIndex}>
+        {row.map((cell, col) => (
+          <div
+            className="box"
+            key={col}
+            onClick={e => this.props.onClick(rowIndex, col)}
+          >
+            {row[col]}
+          </div>
+        ))}
+      </div>
+    ));
+
+    return (
+      <div className="container">
+        <div className="board">{Box}</div>
+      </div>
+    );
+  }
+}
+
+export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
+import Board from "./Board";
 import "./Game.css";
 
 class Game extends Component {
   constructor(props) {
     super(props);
+    // this.handleClick = this.handleClick.bind(this);
     this.state = {
       board: [[null, null, null], [null, null, null], [null, null, null]],
       player: "X"
     };
   }
-  handleClick(row, col) {
+  handleClick = (row, col) => {
     let newBoard = this.state.board;
     newBoard[row][col] = this.state.player;
 
@@ -17,28 +19,38 @@ class Game extends Component {
       board: newBoard,
       player: this.state.player === "X" ? "O" : "X"
     });
-  }
+  };
   render() {
-    const Box = this.state.board.map((row, rowIndex) => (
-      <div className="row" key={rowIndex}>
-        {row.map((cell, col) => (
-          <div
-            className="box"
-            key={col}
-            onClick={e => this.handleClick(rowIndex, col)}
-          >
-            {row[col]}
-          </div>
-        ))}
-      </div>
-    ));
-
     return (
-      <div className="container">
-        <div className="board">{Box}</div>
-      </div>
+      <Board
+        rows={this.state.board}
+        onClick={this.handleClick}
+        player={"X"}
+        newBoard={this.state.player}
+      />
     );
   }
+  // render() {
+  //   const Box = this.state.board.map((row, rowIndex) => (
+  //     <div className="row" key={rowIndex}>
+  //       {row.map((cell, col) => (
+  //         <div
+  //           className="box"
+  //           key={col}
+  //           onClick={e => this.handleClick(rowIndex, col)}
+  //         >
+  //           {row[col]}
+  //         </div>
+  //       ))}
+  //     </div>
+  //   ));
+
+  //   return (
+  //     <div className="container">
+  //       <div className="board">{Box}</div>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Game;
