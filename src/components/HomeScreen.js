4e45f93f14f2ef8c92cb511406c2c901db9c55c6
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-22 12:09:28 -0400

    button design

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,6 +5,7 @@ import Game from "./Game";
 import NewGame from "./NewGame";
 import Credits from "./Credits";
 import { Link } from "react-router-dom";
+import "./HomeScreen.css";
 
 const mapStateToProps = function(state, props) {
   return {};
@@ -16,10 +17,21 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1 className="heading">Tic Tac Toe </h1>
-        <Link to={"/credits"}>Credits</Link>
-        <Link to={"/game"}>Game</Link>
-        <Link to={"/new-game"}>NewGame</Link>
+        <div className="header__text-box">
+          <img
+            className="heading-primary"
+            src={require("../assets/logo.png")}
+          />
+          <Link className="btn btn--white btn--animated" to={"/credits"}>
+            Credits
+          </Link>
+          <Link className="btn btn--white btn--animated" to={"/game"}>
+            Game
+          </Link>
+          <Link className="btn btn--white btn--animated" to={"/new-game"}>
+            NewGame
+          </Link>
+        </div>
 
         {this.props.children}
       </React.Fragment>
